26738414662de523f3c1260a212df6eb01fddc58
SHEIV <daniel.hanic@gmail.com>
2019-07-24 14:55:21 +0200

    Pridanie stranky Kontakt

---
+++ contact.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Kontakt</title>
+  </head>
+  <body>
+    <h1>Kontakt</h1>
+    <ul>
+      <li>0910 100 100</li>
+    </ul>
+  </body>
+</html>
